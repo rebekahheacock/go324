e5595f8b796bc1bc0d4ae277783440b65362411f
Rebekah Heacock Jones <rebekah.heacock@gmail.com>
2016-02-01 19:16:53 -0500

    register new widget area in header

--- functions.php
+++ functions.php
@@ -3,6 +3,19 @@
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+
+function go324_widgets_init() {
+
+	register_sidebar(array('name'=>'Header (Full-width Area)',
+	    'description' => 'Widget area for "Instagram" widget.',
+	    'id' => 'fullwidth_header',
+	    'before_widget' => '<div class="widget %2$s" id="%1$s">',
+	    'after_widget' => '<div class="clear"></div></div>',
+	    'before_title' => '<h3 class="title">',
+	    'after_title' => '</h3>',
+	));
 
 }
+add_action( 'widgets_init', 'go324_widgets_init' );
 
